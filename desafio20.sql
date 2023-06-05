848b5369a633f97785a68aa0d4e7a30afea72175
Douglas Araújo <devdoug45@gmail.com>
2023-06-05 16:28:36 -0300

    feat: reqs 20, 21, 22, 23

---
+++ desafio20.sql
@@ -0,0 +1,3 @@
+-- Active: 1685731352082@@127.0.0.1@3306@northwind
+INSERT INTO order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+  VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
---
+++ desafio22.sql
@@ -0,0 +1,3 @@
+-- Active: 1685731352082@@127.0.0.1@3306@northwind
+UPDATE order_details
+  SET discount = 15;
