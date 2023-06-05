f9cf278f7c6ae783e3005e5a3279938b14eb93f7
Douglas Araújo <devdoug45@gmail.com>
2023-06-05 16:18:04 -0300

    feat: req19, finaliza os desafios de filtragem de dados

--- desafio20.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-
-git remote set-url origin https://{dougaraujo}:{62442M@gic}@github.com/{tryber}/project.git
-
-git remote set-url origin git@github.com:tryber/sd-029-a-mysql-all-for-one.git
